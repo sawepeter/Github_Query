4211156e747c089523b6c55745ad553f2aeb6f99
user.email <sawepeter6@gmail.com>
2018-05-17 03:56:40 +0300

    variable declarations

--- app/src/main/java/com/example/sawepeter/sunshine/MainActivity.java
+++ app/src/main/java/com/example/sawepeter/sunshine/MainActivity.java
@@ -2,8 +2,14 @@ package com.example.sawepeter.sunshine;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText mSearchBoxEditText;
+    TextView mUrlDisplayTextView;
+    TextView mSearchResultsTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
