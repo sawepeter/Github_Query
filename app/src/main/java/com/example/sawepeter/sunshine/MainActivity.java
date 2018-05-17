334f5439c69acdc44537d0e21ac0109205a5d7fe
user.email <sawepeter6@gmail.com>
2018-05-17 04:35:41 +0300

    final mainactivity class

--- app/src/main/java/com/example/sawepeter/sunshine/MainActivity.java
+++ app/src/main/java/com/example/sawepeter/sunshine/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.sawepeter.sunshine;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,13 +24,20 @@ public class MainActivity extends AppCompatActivity {
         mUrlDisplayTextView = (TextView)findViewById(R.id.tv_url_display);
         mSearchResultsTextView = (TextView)findViewById(R.id.tv_github_search_results_json);
     }
-    @Override
+    
     public boolean onCreatOptionsMenu(Menu menu){
         getMenuInflater().inflate(R.menu.menu,menu);
 
         return true;
     }
     public boolean onOptionsItemSelected(MenuItem item){
-        
+        int itemThatWasClickedId = item.getItemId();
+        if (itemThatWasClickedId == R.id.action_search){
+            Context context = MainActivity.this;
+            String textToShow = "Search clicked";
+            Toast.makeText(context, textToShow, Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
