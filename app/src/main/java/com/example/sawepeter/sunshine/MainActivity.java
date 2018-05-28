ac0dce0fc543782b1655fdae612308c24614937a
user.email <sawepeter6@gmail.com>
2018-05-28 15:44:57 +0300

    removal of unused imports 🎨

--- app/src/main/java/com/example/sawepeter/sunshine/MainActivity.java
+++ app/src/main/java/com/example/sawepeter/sunshine/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -42,6 +43,8 @@ public class MainActivity extends AppCompatActivity {
         String githubQuery = mSearchBoxEditText.getText().toString();
         URL githubSearchurl = NetworkUtils.buildUrl(githubQuery);
         mUrlDisplayTextView.setText(githubSearchurl.toString());
+        new GithubQueryTask().execute(githubSearchurl);
+
 
         String githubSearchResults = null;
         try{
@@ -54,6 +57,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void showJsonDataView(){
+        error_msg.setVisibility(View.INVISIBLE);
+
+        mSearchResultsTextView.setVisibility(View.VISIBLE);
 
     }
 
